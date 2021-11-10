1335a379fa624a07b21b9ef3aeff3ebe027b9880
Nguyen Hoang Nguyen <poolaris.vn@gmail.com>
2021-11-10 22:32:15 +0100

    add history

--- src/index.js
+++ src/index.js
@@ -18,31 +18,15 @@ class Square extends React.Component {
 class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.boardSize = 20
-    this.state = {
-      squares: Array(this.boardSize ** 2).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
+    this.boardSize = Math.sqrt(props.squares.length);
   }
 
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
+        value={this.props.squares[i]}
         key={i}
-        onClick={() => this.handleClick(i)}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
@@ -62,16 +46,6 @@ class Board extends React.Component {
   }
 
   render() {
-    let winner = calculateWinner(this.state.squares)
-    let status;
-
-    if (winner) {
-      status = `The winner is ${winner}`
-    } else {
-      status =  `Next player: ${this.state.xIsNext?'X':'O'}`;
-    }
-
-
     let rows = []
     for(let i = 0; i<this.boardSize; i++) {
       let row = this.renderRow(i)
@@ -80,7 +54,7 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="status">{status}</div>
+        {/* <div className="status">{status}</div> */}
         {rows}
       </div>
     );
@@ -88,14 +62,55 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.size = 10
+    this.state = {
+      history: [{
+        squares: Array(this.size ** 2).fill(null)
+      }],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1]
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext?'X':'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+           squares = {current.squares}
+           onClick = {(i)=> this.handleClick(i)}
+           />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
@@ -158,7 +173,6 @@ function checkColumn(squares) {
     for (let ridx = 0; ridx < size; ridx ++) {
       colSquares[ridx] = squares[cidx + ridx * size]
     }
-    
     let {longest, longestVal} = countConsecutiveIdenticalValues(colSquares)
     if(longest == 5) {
       return longestVal
