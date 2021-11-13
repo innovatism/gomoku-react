34fe868b23f9ef538b0d13ec6786a8ce9065b98f
Nguyen Hoang Nguyen <poolaris.vn@gmail.com>
2021-11-13 15:23:53 +0100

    update board size with user's input

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+const WIN_AT = 5;
 
 class Square extends React.Component {
   render() {
@@ -16,9 +17,9 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.boardSize = Math.sqrt(props.squares.length);
+
+  getBoardSize() {
+    return Math.sqrt(this.props.squares.length);
   }
 
   renderSquare(i) {
@@ -33,10 +34,11 @@ class Board extends React.Component {
 
 
   renderRow(i) {
-    let squares = Array(this.boardSize)
+    let boardSize = this.getBoardSize()
+    let squares = Array(boardSize)
 
-    for (let j = 0; j < this.boardSize; j++) {
-      squares[j] = this.renderSquare(this.boardSize * i + j)
+    for (let j = 0; j < boardSize; j++) {
+      squares[j] = this.renderSquare(boardSize * i + j)
     }
     return (
       <div className="board-row" key={i}>
@@ -46,8 +48,9 @@ class Board extends React.Component {
   }
 
   render() {
+    let boardSize = this.getBoardSize()
     let rows = []
-    for(let i = 0; i<this.boardSize; i++) {
+    for (let i = 0; i < boardSize; i++) {
       let row = this.renderRow(i)
       rows.push(row)
     }
@@ -64,10 +67,10 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.size = 20
     this.state = {
+      boardSize: 10,
       history: [{
-        squares: Array(this.size ** 2).fill(null)
+        squares: Array(10 ** 2).fill(null)
       }],
       stepNumber: 0,
       xIsNext: true,
@@ -88,14 +91,30 @@ class Game extends React.Component {
         squares: squares,
       }]),
       stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      
     });
   }
 
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: (step%2) === 0,
+      xIsNext: (step % 2) === 0,
+    })
+  }
+
+  handleValueChange(event) {
+    let {value, min, max} = event.target
+    value = Math.max(Number(min), Math.min(Number(max), Number(value)))
+    value = Math.trunc(value)
+    console.log(`current value ${value}`)
+    this.setState({
+      history: [{
+        squares: Array(value ** 2).fill(null)
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+      boardSize:value?value:"",
     })
   }
 
@@ -103,15 +122,15 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
-
+    
     const moves = history.map((_, move) => {
       const desc = move ?
-      `Go to move #${move}`:
-      `Go to game start`;
+        `Go to move #${move}` :
+        `Go to game start`;
 
       return (
         <li key={move}>
-          <button onClick = {() => this.jumpTo(move)}>
+          <button onClick={() => this.jumpTo(move)}>
             {desc}
           </button>
         </li>
@@ -122,18 +141,27 @@ class Game extends React.Component {
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
-      status = 'Next player: ' + (this.state.xIsNext?'X':'O');
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-
+    console.log(`current + ${current.squares}`)
     return (
       <div className="game">
         <div className="game-board">
-          <Board 
-           squares = {current.squares}
-           onClick = {(i)=> this.handleClick(i)}
-           />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
+          <label>
+            Board Size:
+          </label>
+          <input value={this.state.boardSize}
+            onChange={(event) => this.handleValueChange(event)}
+            type="number"
+            min="0"
+            max="20"
+          />
           <div>{status}</div>
           <ol>{moves}</ol>
         </div>
@@ -176,15 +204,15 @@ function countConsecutiveIdenticalValues(aArray) {
       currentCount = 1;
     }
   }
-  return {longest, longestVal};
+  return { longest, longestVal };
 }
 
 function checkRow(squares) {
   let size = Math.sqrt(squares.length)
   for (let ridx = 0; ridx < size; ridx++) {
-    let rowSquares = squares.slice(ridx * size, (ridx+1) * size )
-    let {longest, longestVal} = countConsecutiveIdenticalValues(rowSquares)
-    if(longest === 5) {
+    let rowSquares = squares.slice(ridx * size, (ridx + 1) * size)
+    let { longest, longestVal } = countConsecutiveIdenticalValues(rowSquares)
+    if (longest === WIN_AT) {
       return longestVal
     }
   }
@@ -194,11 +222,11 @@ function checkColumn(squares) {
   let size = Math.sqrt(squares.length)
   for (let cidx = 0; cidx < size; cidx++) {
     let colSquares = Array(size).fill(null)
-    for (let ridx = 0; ridx < size; ridx ++) {
+    for (let ridx = 0; ridx < size; ridx++) {
       colSquares[ridx] = squares[cidx + ridx * size]
     }
-    let {longest, longestVal} = countConsecutiveIdenticalValues(colSquares)
-    if(longest == 5) {
+    let { longest, longestVal } = countConsecutiveIdenticalValues(colSquares)
+    if (longest == 5) {
       return longestVal
     }
   }
@@ -209,7 +237,7 @@ function generateTLRBiagonals(size) {
   for (let s = 0; s < 2 * size - 1; s++) {
     let diag = []
     for (let cidx = 0; cidx < size; cidx++) {
-      for (let ridx = size -1; ridx >= 0; ridx--) {
+      for (let ridx = size - 1; ridx >= 0; ridx--) {
         if (cidx + ridx === s) {
           diag.push([cidx, ridx])
         }
@@ -226,7 +254,7 @@ function generateBLTRDiagonals(size) {
   for (let d = -(size - 1); d < size; d++) {
     let diag = []
     for (let cidx = 0; cidx < size; cidx++) {
-      for (let ridx = 0; ridx <size; ridx++) {
+      for (let ridx = 0; ridx < size; ridx++) {
         if (cidx - ridx === d) {
           diag.push([cidx, ridx])
         }
@@ -241,7 +269,7 @@ function checkDiagonal(squares) {
   let size = Math.sqrt(squares.length);
   let idxDiags = generateBLTRDiagonals(size);
   idxDiags.push(...generateTLRBiagonals(size))
-  for(let i=0; i<idxDiags.length; i++) {
+  for (let i = 0; i < idxDiags.length; i++) {
     let diag = idxDiags[i].map((idx) => {
       let [cidx, ridx] = idx;
       return squares[cidx + ridx * size]
@@ -256,13 +284,10 @@ function checkDiagonal(squares) {
 function calculateWinner(squares) {
   console.time("checkWinner")
   let longestVal = checkRow(squares)
-  console.log(longestVal)
   if (longestVal == null) {
     longestVal = checkColumn(squares)
-    console.log(longestVal)
     if (longestVal == null) {
       longestVal = checkDiagonal(squares)
-      console.log(longestVal)
     }
   }
   console.timeEnd("checkWinner")
